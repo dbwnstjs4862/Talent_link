8ebc614d30b65b884919b3d1c3feb180df3a91e5
Yoo Junseon <dbwnstjs4862@gmail.com>
2025-05-18 19:01:26 +0900

    초기 커밋: 프로젝트 구조 설정

---
+++ src/test/java/com/talentlink/talentlink/TalentlinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.talentlink.talentlink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TalentlinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
